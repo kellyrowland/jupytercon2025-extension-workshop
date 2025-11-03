b7da08807573a74797026af20a38c090e76374d1
Kelly Rowland <kellylynnerowland@gmail.com>
2025-11-03 12:10:17 -0800

    add image and caption endpoint

---
+++ jupytercon2025_extension_workshop/images_and_captions.py
@@ -0,0 +1,6 @@
+# Public domain images from https://www.loc.gov/free-to-use/
+IMAGES_AND_CAPTIONS = [
+    { "filename": "00507v.jpg", "caption": "A Grove of Cherry Trees" },
+    { "filename": "01307v.jpg", "caption": "Sunset Hill, Meguro in the eastern capitol" },
+    { "filename": "01317v.jpg", "caption": "Pine beach at Miho in Suruga" },
+]
